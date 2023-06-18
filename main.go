a8c970dafd27013b9c9550019a01aa40edf1e024
Yehdar <p.radhey06@gmail.com>
2023-06-18 15:56:36 -0400

    added os module to relay user input to operating system

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main(){
-	fmt.Printf("hollup... Let hiim cook")
+	if len(os.Args) < 3  {
+		fmt.Printf("Usage:%s <time> <message>\n", os.Args[0])
+		os.Exit(1)
+	}
 }
