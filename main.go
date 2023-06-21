5633c6cfe078d9e4a92e3b64b8777bf67535cf40
Yehdar <p.radhey06@gmail.com>
2023-06-21 13:16:43 -0400

    made it user-friendly

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"strings"
 	"time"
 
 	"github.com/gen2brain/beeep"
@@ -14,15 +13,16 @@ import (
 )
 
 const (
-	markName = "hey_thats_me"
-	markValue = "1"
+	envVar = "hey_thats_me"
+	envVal = "1"
 )
 
 func main(){
-	if len(os.Args) < 3  {
-		fmt.Printf("Usage:%s <time> <message>\n", os.Args[0])
-		os.Exit(1)
-	}
+	var userInputTime, userInputMsg string
+	fmt.Println("Hey there! Want to send a reminder to your future self?")
+	fmt.Print("Drop the time <hh:mm> and message: ")
+	fmt.Scan(&userInputTime, &userInputMsg)
+	fmt.Println(userInputTime + userInputMsg)
 
 	now := time.Now()
 	
@@ -30,7 +30,7 @@ func main(){
 	w.Add(en.All...)
 	w.Add(common.All...)
 
-	t, err := w.Parse(os.Args[1], now)
+	t, err := w.Parse(userInputTime, now)
 
 	if t == nil {
 		fmt.Println("Can't parse time!")
@@ -43,21 +43,21 @@ func main(){
 	}
 
 	if now.After(t.Time){
-		fmt.Println("pick a time in the future!")
+		fmt.Println("Pick a time in the future!")
 		os.Exit(3)
 	}
 
 	timeDiff := t.Time.Sub(now)
-	if os.Getenv(markName) == markValue{
+	if os.Getenv(envVar) == envVal{
 		time.Sleep(timeDiff)
-		err = beeep.Alert("Reminder", strings.Join(os.Args[2:], " "), "assets/information.png" )
+		err = beeep.Alert("Reminder", userInputMsg, "assets/information.png" )
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(4)
 		}
 	} else {
-		cmd := exec.Command(os.Args[0], os.Args[1:]...)
-		cmd.Env = append(os.Environ(), fmt.Sprintf("%s=%s", markName, markValue))
+		cmd := exec.Command(os.Args[0], userInputTime, userInputMsg)
+		cmd.Env = append(os.Environ(), fmt.Sprintf("%s=%s", envVar, envVal))
 		if err := cmd.Start(); err != nil {
 			fmt.Println(err)
 			os.Exit(5)
